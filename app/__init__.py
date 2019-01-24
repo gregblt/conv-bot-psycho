09925b8994f447c58f499ad966057d90eb9c4381
gregblt <gregorybliault@gmail.com>
2019-01-24 01:04:54 +0100

    add socket io requirement

--- app/__init__.py
+++ app/__init__.py
@@ -21,39 +21,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 db = SQLAlchemy(app)
 
-from flask_socketio import SocketIO
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
-socketio = SocketIO(app)
-
-clients = {};
-
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
-@socketio.on('connect_master')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: '+ str(json))
-    socketio.emit('my response', json, callback=messageReceived)
-
-@socketio.on('connect_user')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: '+ str(json))
-    uuid_user = str(uuid.uuid4());
-    clients[request.sid]=(request.namespace);
-    socketio.emit('user_connected', {'uuid':uuid_user}, callback=messageReceived)
-
-@socketio.on('new_user_msg')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: '+ str(json))
-    print(request.sid)
-    json['sid'] = request.sid
-    socketio.emit('to_master', json, callback=messageReceived)
-
-@socketio.on('new_master_msg')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: '+ str(json))
-    socketio.emit('to_client', json, callback=messageReceived, room=json['user'])
-
 
 from app.models import Conversations
 
